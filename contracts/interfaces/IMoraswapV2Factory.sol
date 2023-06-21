437c5e321bde2a74a6e204b0588086f984592ea1
0xkaidev <0xkai.dev@gmail.com>
2023-06-21 08:21:10 +0700

    init

---
+++ contracts/interfaces/IMoraswapV2Factory.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12;
+
+interface IMoraswapV2Factory {
+    function feeTo() external view returns (address);
+
+    function feeToSetter() external view returns (address);
+
+    function migrator() external view returns (address);
+
+    function getPair(address tokenA, address tokenB) external view returns (address pair);
+
+    function allPairs(uint256) external view returns (address pair);
+
+    function allPairsLength() external view returns (uint256);
+
+    function createPair(address tokenA, address tokenB) external returns (address pair);
+
+    function setFeeTo(address) external;
+
+    function setFeeToSetter(address) external;
+
+    function setMigrator(address) external;
+}
